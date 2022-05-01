f903c27f3320998c57c4476674002d1ffb0f8a59
Horia Mocanu <79640015+horia-mocanu@users.noreply.github.com>
2022-05-01 23:56:30 +0300

    Add files via upload

---
+++ ProjectPAO/ProjectPAO/src/Main.java
@@ -0,0 +1,8 @@
+import Service.ServiceClass;
+
+public class Main {
+    public static void main(String[] args) {
+        ServiceClass app = ServiceClass.getInstance();
+        app.Run();
+    }
+}
