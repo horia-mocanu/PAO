67ded8d7ef468f6e8c09a69b0b950189da26f056
Horia Mocanu <79640015+horia-mocanu@users.noreply.github.com>
2022-05-25 22:26:22 +0300

    Delete ProjectPAO directory

--- ProjectPAO/ProjectPAO/src/Main.java
+++
@@ -1,8 +0,0 @@
-import Service.ServiceClass;
-
-public class Main {
-    public static void main(String[] args) {
-        ServiceClass app = ServiceClass.getInstance();
-        app.Run();
-    }
-}
